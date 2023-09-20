77b16732977256baa0c31ab4e31992891296746a
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-20 15:04:35 +0800

    Add rows div to html
    Added rows div to html so createSquare can be appended inside it.

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 // input: prompt 1-100
 // for loop to generate boxes depending on input
+    // loop rows
     // loop columns
     // function that creates empty divs
-    // loop rows
 // output: boxes generated depending on input
 
 function createSquare() {
     const square = document.createElement("div");
     square.classList.add("box");
-    document.querySelector(".container").appendChild(square);
+    document.querySelector(".row").appendChild(square);
 }
 
 function createRow() {
-    const column = document.createElement("div");
+    const row = document.createElement("div");
     document.querySelector(".container").appendChild(column);
 }
-
-// loop to create rows
-// for (let i = 0; i <= 16; i++) {
-//    createSquare();
-// }
