bc520213fb2584989f27b4c6bcbf8b6b21346898
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-20 14:28:02 +0800

    Add createSquare function
    Added createSquare function that creates a new div inside container.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // let i = 0, loop until i = 16
     // for j = 0, loop until j = 16
 
-// loop columns
-// function that creates empty divs
-// loop rows
+// input: prompt 1-100
+// for loop to generate boxes depending on input
+    // loop columns
+    // function that creates empty divs
+    // loop rows
+// output: boxes generated depending on input
+
+function createSquare() {
+    const square = document.createElement("div");
+    square.classList.add("box");
+    document.querySelector(".container").appendChild(square);
+}
