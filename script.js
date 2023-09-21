83652a013f2e60d41089b1a53ed934ce5f6ef949
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-09-21 10:19:32 +0800

    Update createSquare function
    Used querySelectAll instead of querySelect inside createSquare function.
    Used forEach to loop the nodelist from querySelectAll.
    So, all rows can be appended with square. Not just the first row.

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-// input: prompt 1-100
-// for loop to generate boxes depending on input
-    // loop rows
-    // loop columns
-    // function that creates empty divs
-// output: boxes generated depending on input
-
 function createSquare() {
     const square = document.createElement("div");
+    const rows = document.querySelectorAll(".row");
+
     square.classList.add("box");
-    document.querySelector(".row").appendChild(square);
+    rows.forEach((row) => {
+        row.appendChild(square);
+    });
 }
 
 
-// loop to create each row
 for (let i = 0; i <= 5; i++) {
-    // create row
     const row = document.createElement("div");
     row.classList.add("row");
     document.querySelector(".container").appendChild(row);
 
-    // loop to create the squares inside each row
     for (let j = 0; j <= 5; j++) {
         createSquare();
     }
